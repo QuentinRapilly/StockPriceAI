ab0af7805b2594503b9a233e8fcf3aa648ffe5c8
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2021-11-24 18:21:48 +0100

    [+] premier commit

---
+++ StockAI.py
@@ -0,0 +1,20 @@
+from torch.nn import LSTM,Module,Dropout,ModuleList
+
+class StockAI(Module):
+     
+    def __init__(self, input_size, lstm_size, num_layers, keep_prob) -> None:
+        super().__init__()
+        self.input_size = input_size
+        self.lstm_size = lstm_size
+        self.num_layers = num_layers
+        self.lstm_list = ModuleList([LSTM(self.input_size, self.lstm_size) for _ in range(self.num_layers)])
+        self.dropout_list = ModuleList([Dropout() for _ in range(self.num_layers)])
+
+    
+    def foward(self,x):
+        y = x
+        for i in range(self.num_layers):
+            y = self.lstm_list[i](x)
+            y = self.dropout_list[i](x)
+        
+        return y
---
+++ stock_price_prediction_1.py
@@ -0,0 +1,1 @@
+from StockAI import StockAI
